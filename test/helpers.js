b192b31350206bf07c1ee38a9debc5ca13922ba5
Ian Johnson <ian@ianjohnson.dev>
2022-03-04 20:23:29 -0500

    test: add type-check test

--- test/helpers.js
+++ test/helpers.js
@@ -2,6 +2,10 @@ import * as assert from "uvu/assert";
 
 import { RawHtml } from "../index.js";
 
+/**
+ * @param {unknown} actual
+ * @param {RawHtml} expects
+ */
 export function assertIsRawHtml(actual, expects) {
   assert.ok(actual instanceof RawHtml, `expected RawHtml`);
   assert.equal(actual.html, expects, "raw HTML does not match");
